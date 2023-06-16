416dc751bdab0bb08ec56ab5f11d58585ef818ad
Serkan Aydın <serkanydn96@gmail.com>
2023-06-16 12:00:50 +0300

    landing page 1 import name problem fixed

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/mainLayout";
 import ShoesCard from "../components/shoesCard";
 import CookiePreferencesModal from "../components/modals/cookiePreferencesModal";
-import LandingPage1 from "../landingpages/landingpage1";
+import LandingPage1 from "../landingPages/landingPage1";
 
 const router = createBrowserRouter([
   {
