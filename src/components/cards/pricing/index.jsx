d72014f79aeeaf61628264e7febe9eb5b95e4e90
serkanydn <serkanydn96@gmail.com>
2023-07-18 22:48:27 +0300

    refactor: pricing card radio button problem fixed

--- src/components/cards/pricing/index.jsx
+++ src/components/cards/pricing/index.jsx
@@ -11,31 +11,42 @@ function Pricing() {
           <p>We uncover the facts around the clock, all over the globe.</p>
         </div>
 
-        <div className="cards">
-          <div className="card">
-            <input type="radio" checked name="radio-group" id="" value="ss" />
+        <form className="cards">
+          <label className="card" for="radio-1">
+            <input
+              type="radio"
+              defaultChecked 
+              name="radio-group"
+              id="radio-1"
+              value="radio-1"
+            />
             <div className="content">
               <h4>Monthly</h4>
               <p>4 weeks for free</p>
               <p>Then $3 every month for the first year</p>
             </div>
-          </div>
+          </label>
 
-          <div className="card">
-            <input type="radio" name="radio-group" id="" />
+          <label className="card" for="radio-2">
+            <input
+              type="radio"
+              name="radio-group"
+              id="radio-2"
+              value="radio-2"
+            />
             <div className="content">
               <span>Best Value</span>
               <h4>Yearly</h4>
               <p>4 weeks for free</p>
               <p>Then $30 for the first year</p>
             </div>
-          </div>
-        </div>
+          </label>
+        </form>
 
         <div className="buttons">
           <button className="btn btn-primary">Continue</button>
           <button className="btn-outlined btn-icon">
-            Continue With <img src={paypalSvg} alt="" />{" "}
+            Continue With <img src={paypalSvg} alt="" />
           </button>
           <button className="btn btn-link">View more offers </button>
         </div>
